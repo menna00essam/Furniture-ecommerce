a272867f75ca4012b2f0c7614ef45cf01a74a4b8
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-16 14:42:59 +0200

    configured tailwind theme

--- client-side/tailwind.config.ts
+++ client-side/tailwind.config.ts
@@ -3,16 +3,7 @@ import { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        noto: ['Noto Sans', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-      colors: {
-        cream: '#FCF8F3', // Adding the custom color
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
