d645828bb56649b25da84650e7a57290a9b0e4b2
mariemhawary <mariemhawary843@gmail.com>
2025-02-18 22:07:30 +0200

    handle hero section and shared button

--- client-side/src/app/Components/home/home.component.ts
+++ client-side/src/app/Components/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
+import { UpperCasePipe } from '@angular/common';
+import { ButtonComponent } from '../../shared/button/button.component';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonComponent,UpperCasePipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ client-side/src/app/shared/button/button.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input() type: 'filledBtn' | 'outlinedBtn' = 'filledBtn';
+  @Input() btnPadding: string = '10px';
+  @Input() btnWidth: string = '150px';
+}
