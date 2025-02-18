ed711e3b984543012514532720839e3effd9ff66
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-18 13:16:46 +0200

    implemented features banner

--- client-side/src/app/Components/shop/shop.component.ts
+++ client-side/src/app/Components/shop/shop.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FeatureBannerComponent } from "../feature-banner/feature-banner.component";
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [FeatureBannerComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
