97dc5fb0a1434ec166b638627a4d78cd4994f0d7
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-20 22:21:48 +0200

    added products items in shop

--- client-side/src/app/Components/shop/shop.component.ts
+++ client-side/src/app/Components/shop/shop.component.ts
@@ -5,6 +5,9 @@ import { HeaderBannerComponent } from '../header-banner/header-banner.component'
 import { CommonModule } from '@angular/common';
 import { DropdownComponent } from '../dropdown/dropdown.component';
 import { FilterOptionComponent } from '../filter-option/filter-option.component';
+import { ProductItemComponent } from '../../shared/product-item/product-item.component';
+import { product } from '../../models/product.model';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-shop',
@@ -15,6 +18,8 @@ import { FilterOptionComponent } from '../filter-option/filter-option.component'
     CommonModule,
     DropdownComponent,
     FilterOptionComponent,
+    ProductItemComponent,
+    ButtonComponent,
   ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css',
@@ -22,7 +27,7 @@ import { FilterOptionComponent } from '../filter-option/filter-option.component'
 export class ShopComponent {
   searchValue: string = 'Default';
   showFilters: boolean = true;
-  
+
   menuItems: string[] = [
     'Default',
     'Price: Low to High',
@@ -30,6 +35,67 @@ export class ShopComponent {
     'Rating',
     'Newest',
     'Discount',
+  ];
+
+  products: product[] = [
+    {
+      id: 1,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+      sale: 20,
+    },
+    {
+      id: 2,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 3,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 4,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+      sale: 20,
+    },
+    {
+      id: 5,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 6,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 7,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 8,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
   ];
 
   toggleShowFilter() {
