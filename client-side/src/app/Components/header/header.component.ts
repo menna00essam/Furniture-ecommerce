2a50417be6cd65285604df8c5284d334c78fb951
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-22 21:59:47 +0200

    added modal for favorites and cart

--- client-side/src/app/Components/header/header.component.ts
+++ client-side/src/app/Components/header/header.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   isActive = false;
+  favModalShow = false;
+  cartModalShow = false;
 
   toggleMenu() {
     this.isActive = !this.isActive;
+  }
+  toggleFavModal(open: boolean) {
+    this.favModalShow = open;
+    if (open) {
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'auto';
+    }
+  }
+  toggleCartModal(open: boolean) {
+    this.cartModalShow = open;
+    if (open) {
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'auto';
+    }
   }
 }
