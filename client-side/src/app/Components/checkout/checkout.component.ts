16f288f52242ec34b3d49e4b2f7ca0d6a68a0917
ahmedgamal5 <ahmedgamalflame5534@gmail.com>
2025-02-22 17:06:58 +0200

    Add checkout components

--- client-side/src/app/Components/checkout/checkout.component.ts
+++ client-side/src/app/Components/checkout/checkout.component.ts
@@ -1,13 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
-  imports: [CommonModule, FormsModule],
+  imports: [],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css',
+  styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
-  selectedPayment: string = '';
+
 }
