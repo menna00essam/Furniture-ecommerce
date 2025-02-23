3e8d0ad71dd62211f24151f8d220eab3decedc34
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-23 20:56:49 +0200

    fixed blogs pages spacing and responsivity

--- client-side/src/app/Components/blogs/blogs.component.ts
+++ client-side/src/app/Components/blogs/blogs.component.ts
@@ -5,15 +5,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderBannerComponent } from '../header-banner/header-banner.component';
 import { FeatureBannerComponent } from '../feature-banner/feature-banner.component';
+import { DropdownComponent } from '../dropdown/dropdown.component';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
-  selector: 'app-blog',
+  selector: 'app-blogs',
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
     HeaderBannerComponent,
     FeatureBannerComponent,
+    DropdownComponent,
+    ButtonComponent,
   ],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css',
@@ -44,7 +48,7 @@ export class BlogsComponent {
         'Discover how to refresh your living space with stylish and comfortable furniture choices.',
       content:
         "Are you tired of the same old living room setup, longing for a refreshing change? It's time to embark on a journey of transformation with our meticulously curated selection of stylish furniture. We understand the importance of creating a living space that not only exudes comfort but also captivates with its aesthetic allure...",
-      image: '/images/desgin1.jpg',
+      image: '/images/Blogs/1.jpg',
       category: 'Furniture & Decor',
       date: '2024-02-18',
       users: 'InteriorDesignPro',
@@ -56,7 +60,7 @@ export class BlogsComponent {
         'Your dining space is more than just a place to eat; it’s a hub for connection and style. Learn how to choose the perfect furniture.',
       content:
         'The dining room is a multifaceted space that serves various purposes beyond just a place to eat; it’s a central hub for gathering, entertaining, and creating enduring memories. Selecting the right dining furniture is paramount to elevating this experience...',
-      image: '/images/desgin2.jpg',
+      image: '/images/Blogs/4.jpg',
       category: 'Furniture & Decor',
       date: '2024-02-17',
       users: 'InteriorDesignPro',
@@ -68,7 +72,7 @@ export class BlogsComponent {
         'Your bedroom should be a sanctuary of comfort and relaxation. Explore tips for selecting the right furniture and decor.',
       content:
         'Your bedroom should be the most relaxing space in your home, offering a perfect blend of comfort and style. From selecting the right bed frame to choosing soothing color palettes, every detail contributes to a serene and inviting atmosphere...',
-      image: '/images/desgin3.jpg',
+      image: '/images/Blogs/2.jpg',
       category: 'Furniture & Decor',
       date: '2024-02-16',
       users: 'DiningRoomGuru',
@@ -80,7 +84,7 @@ export class BlogsComponent {
         'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
       content:
         'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/desgin4.jpg',
+      image: './images/Blogs/3.jpg',
       category: 'Furniture',
       date: '2024-02-16',
       users: 'SleepWellDesigner',
@@ -92,7 +96,7 @@ export class BlogsComponent {
         'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
       content:
         'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/carfts1.jpg',
+      image: './images/Blogs/5.jpg',
       category: 'office',
       date: '2024-02-16',
       users: 'SleepWellDesigner',
@@ -104,7 +108,7 @@ export class BlogsComponent {
         'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
       content:
         'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/carfts2.jpg',
+      image: './images/Blogs/6.jpg',
       category: 'office',
       date: '2024-02-16',
       users: 'SleepWellDesigner',
@@ -116,7 +120,7 @@ export class BlogsComponent {
         'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
       content:
         'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/carfts3.jpg',
+      image: './images/Blogs/7.jpg',
       category: 'office',
       date: '2024-02-16',
       users: 'SleepWellDesigner',
@@ -128,7 +132,7 @@ export class BlogsComponent {
         'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
       content:
         'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/carft4.jpg',
+      image: './images/Blogs/8.jpg',
       category: 'smart home',
       date: '2024-02-16',
       users: 'SleepWellDesigner',
@@ -140,7 +144,7 @@ export class BlogsComponent {
         'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
       content:
         'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/carfts2.jpg',
+      image: './images/Blogs/9.jpg',
       category: 'smart home',
       date: '2024-02-16',
       users: 'SleepWellDesigner',
