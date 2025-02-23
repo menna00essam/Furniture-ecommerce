d19f021942946d28022a83d06118104503d412bd
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-23 02:13:47 +0200

    Implemented cart and favourites modal

--- client-side/src/app/Components/header/header.component.ts
+++ client-side/src/app/Components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '../../shared/button/button.component';
-
+import { product } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, ButtonComponent],
+  imports: [RouterModule, ButtonComponent, CurrencyPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
@@ -12,6 +13,94 @@ export class HeaderComponent {
   isActive = false;
   favModalShow = false;
   cartModalShow = false;
+  favorites: product[] = [
+    {
+      id: 1,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+      sale: 20,
+    },
+  ];
+  cart: product[] = [
+    {
+      id: 1,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+      sale: 20,
+    },
+  ];
+
+  products: product[] = [
+    {
+      id: 1,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+      sale: 20,
+    },
+    {
+      id: 2,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 3,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 4,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+      sale: 20,
+    },
+    {
+      id: 5,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 6,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 7,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+    {
+      id: 8,
+      img: '/images/product.png',
+      title: 'Syltherine',
+      discription: 'Stylish cafe chair',
+      price: 2500000,
+    },
+  ];
+  cartProductsTotalPrice = 0;
+  constructor() {
+    this.cartProductsTotalPrice = this.cart.reduce(
+      (total, i) => total + i.price,
+      0
+    );
+  }
 
   toggleMenu() {
     this.isActive = !this.isActive;
@@ -20,16 +109,30 @@ export class HeaderComponent {
     this.favModalShow = open;
     if (open) {
       document.body.style.overflowY = 'hidden';
+      document.body.style.width = 'calc(100% - 10px)';
     } else {
       document.body.style.overflowY = 'auto';
+      document.body.style.width = '';
     }
   }
   toggleCartModal(open: boolean) {
     this.cartModalShow = open;
     if (open) {
       document.body.style.overflowY = 'hidden';
+      document.body.style.width = 'calc(100% - 10px)';
     } else {
       document.body.style.overflowY = 'auto';
+      document.body.style.width = '';
     }
+  }
+  deletefavorites(id: number) {
+    this.favorites = this.favorites.filter((fav) => fav.id != id);
+  }
+  deleteCartProduct(id: number) {
+    this.cart = this.cart.filter((fav) => fav.id != id);
+    this.cartProductsTotalPrice = this.cart.reduce(
+      (total, i) => total + i.price,
+      0
+    );
   }
 }
