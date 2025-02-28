d501f10cd924197136b27e1f1497b5ed20700924
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-02-28 20:48:32 +0200

    added blog service

--- client-side/src/app/Components/blogs/blogs.component.ts
+++ client-side/src/app/Components/blogs/blogs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { BlogPost } from '../../models/blog-post';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,8 @@ import { HeaderBannerComponent } from '../header-banner/header-banner.component'
 import { FeatureBannerComponent } from '../feature-banner/feature-banner.component';
 import { DropdownComponent } from '../dropdown/dropdown.component';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { PaginationComponent } from '../pagination/pagination.component';
+import { BlogService } from '../../Services/blog.service';
 
 @Component({
   selector: 'app-blogs',
@@ -18,11 +20,13 @@ import { ButtonComponent } from '../../shared/button/button.component';
     FeatureBannerComponent,
     DropdownComponent,
     ButtonComponent,
+    PaginationComponent,
   ],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css',
 })
-export class BlogsComponent {
+export class BlogsComponent implements OnInit {
+  @ViewChild('blogsContainer') blogsContainer!: ElementRef;
   categories: string[] = [
     'All',
     'Furniture & Decor',
@@ -40,121 +44,18 @@ export class BlogsComponent {
   isMenuOpen: boolean = false;
   searchQuery: string = '';
   post?: BlogPost;
-  posts: BlogPost[] = [
-    {
-      id: 1,
-      title: 'Transform Your Living Room with Elegant Furniture',
-      excerpt:
-        'Discover how to refresh your living space with stylish and comfortable furniture choices.',
-      content:
-        "Are you tired of the same old living room setup, longing for a refreshing change? It's time to embark on a journey of transformation with our meticulously curated selection of stylish furniture. We understand the importance of creating a living space that not only exudes comfort but also captivates with its aesthetic allure...",
-      image: '/images/Blogs/1.jpg',
-      category: 'Furniture & Decor',
-      date: '2024-02-18',
-      users: 'InteriorDesignPro',
-    },
-    {
-      id: 2,
-      title: 'Dining Room Makeover: The Perfect Table & Chairs',
-      excerpt:
-        'Your dining space is more than just a place to eat; it’s a hub for connection and style. Learn how to choose the perfect furniture.',
-      content:
-        'The dining room is a multifaceted space that serves various purposes beyond just a place to eat; it’s a central hub for gathering, entertaining, and creating enduring memories. Selecting the right dining furniture is paramount to elevating this experience...',
-      image: '/images/Blogs/4.jpg',
-      category: 'Furniture & Decor',
-      date: '2024-02-17',
-      users: 'InteriorDesignPro',
-    },
-    {
-      id: 3,
-      title: 'Bedroom Bliss: Designing a Cozy and Stylish Retreat',
-      excerpt:
-        'Your bedroom should be a sanctuary of comfort and relaxation. Explore tips for selecting the right furniture and decor.',
-      content:
-        'Your bedroom should be the most relaxing space in your home, offering a perfect blend of comfort and style. From selecting the right bed frame to choosing soothing color palettes, every detail contributes to a serene and inviting atmosphere...',
-      image: '/images/Blogs/2.jpg',
-      category: 'Furniture & Decor',
-      date: '2024-02-16',
-      users: 'DiningRoomGuru',
-    },
-    {
-      id: 4,
-      title: 'The Magic of Lighting: Enhance Your Home Ambience',
-      excerpt:
-        'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
-      content:
-        'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/Blogs/3.jpg',
-      category: 'Furniture',
-      date: '2024-02-16',
-      users: 'SleepWellDesigner',
-    },
-    {
-      id: 5,
-      title: 'The Magic of Lighting: Enhance Your Home Ambience',
-      excerpt:
-        'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
-      content:
-        'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/Blogs/5.jpg',
-      category: 'office',
-      date: '2024-02-16',
-      users: 'SleepWellDesigner',
-    },
-    {
-      id: 6,
-      title: 'The Magic of Lighting: Enhance Your Home Ambience',
-      excerpt:
-        'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
-      content:
-        'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/Blogs/6.jpg',
-      category: 'office',
-      date: '2024-02-16',
-      users: 'SleepWellDesigner',
-    },
-    {
-      id: 7,
-      title: 'The Magic of Lighting: Enhance Your Home Ambience',
-      excerpt:
-        'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
-      content:
-        'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/Blogs/7.jpg',
-      category: 'office',
-      date: '2024-02-16',
-      users: 'SleepWellDesigner',
-    },
-    {
-      id: 8,
-      title: 'The Magic of Lighting: Enhance Your Home Ambience',
-      excerpt:
-        'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
-      content:
-        'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/Blogs/8.jpg',
-      category: 'smart home',
-      date: '2024-02-16',
-      users: 'SleepWellDesigner',
-    },
-    {
-      id: 9,
-      title: 'The Magic of Lighting: Enhance Your Home Ambience',
-      excerpt:
-        'Lighting can transform your space. Learn how to use floor lamps, pendant lights, and smart lighting solutions to set the mood.',
-      content:
-        'Lighting plays a crucial role in home decor, shaping the ambiance and functionality of each room. Whether it’s warm-toned pendant lights for the living room or task lighting for the workspace, the right choices can elevate your home’s atmosphere...',
-      image: './images/Blogs/9.jpg',
-      category: 'smart home',
-      date: '2024-02-16',
-      users: 'SleepWellDesigner',
-    },
-  ];
-
-  constructor(private readonly route: ActivatedRoute) {}
-
+  posts: BlogPost[] = [];
   currentPage: number = 1;
   postsPerPage: number = 3;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private blogService: BlogService
+  ) {}
+
+  ngOnInit() {
+    this.posts = this.blogService.getBlogs();
+  }
 
   get totalPages(): number {
     return Math.ceil(this.filteredPosts.length / this.postsPerPage);
@@ -167,33 +68,6 @@ export class BlogsComponent {
     return this.filteredPosts.slice(start, end);
   }
 
-  nextPage(): void {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-  }
-
-  prevPage(): void {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-  }
-
-  goToPage(page: number): void {
-    this.currentPage = page;
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-
-  ngOnInit() {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-
-    if (!isNaN(id)) {
-      this.post = this.posts.find((p) => p.id === id);
-    }
-  }
-
   get filteredPosts(): BlogPost[] {
     return this.posts.filter(
       (post) =>
@@ -202,6 +76,23 @@ export class BlogsComponent {
         (this.searchQuery === '' ||
           post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
     );
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+
+    if (this.blogsContainer) {
+      const offset = 100;
+      const topPosition =
+        this.blogsContainer.nativeElement.getBoundingClientRect().top +
+        window.scrollY -
+        offset;
+
+      window.scrollTo({
+        top: topPosition,
+        behavior: 'smooth',
+      });
+    }
   }
 
   filterByCategory(category: string) {
