8951c1d8737c30505b9777d6cd2a7cdde8aeb88b
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-02 04:45:42 +0200

    used currency pipe

--- client-side/src/app/Components/cart/cart-components/cart-totals/cart-totals.component.ts
+++ client-side/src/app/Components/cart/cart-components/cart-totals/cart-totals.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '../../../../shared/button/button.component';
 
@@ -8,7 +8,7 @@ import { ButtonComponent } from '../../../../shared/button/button.component';
   templateUrl: './cart-totals.component.html',
   styleUrls: ['./cart-totals.component.css'],
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, CurrencyPipe],
 })
 export class CartTotalsComponent {
   constructor(private router: Router) {}
