6e364ed0998dd84d8d34d0247c4b3bebdaf2039a
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-02 06:15:07 +0200

    created not found page

--- client-side/src/app/Components/not-found/not-found.component.ts
+++ client-side/src/app/Components/not-found/not-found.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [ButtonComponent, RouterModule],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
