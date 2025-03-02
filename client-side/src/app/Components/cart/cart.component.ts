859442671f7c3b2cf26c7c61f9ecb4ff41439a66
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-02 04:46:13 +0200

    added cart service to cart page

--- client-side/src/app/Components/cart/cart.component.ts
+++ client-side/src/app/Components/cart/cart.component.ts
@@ -1,17 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CartTotalsComponent } from './cart-components/cart-totals/cart-totals.component';
 import { FeatureBannerComponent } from '../feature-banner/feature-banner.component';
 import { HeaderBannerComponent } from '../header-banner/header-banner.component';
-
-interface CartItem {
-  id: number;
-  name: string;
-  price: number;
-  quantity: number;
-  image: string;
-}
+import { CartService } from '../../Services/cart.service';
+import { product } from '../../models/product.model';
 
 @Component({
   selector: 'app-cart',
@@ -24,36 +18,28 @@ interface CartItem {
     CartTotalsComponent,
     HeaderBannerComponent,
     FeatureBannerComponent,
+    CurrencyPipe,
   ],
 })
-export class CartComponent {
-  cart: CartItem[] = [
-    {
-      id: 1,
-      name: 'Asgaard sofa',
-      price: 250000,
-      quantity: 1,
-      image: 'images/mainsofa.png',
-    },
-    {
-      id: 2,
-      name: 'Asgaard sofa',
-      price: 250000,
-      quantity: 1,
-      image: 'images/mainsofa.png',
-    },
-  ];
+export class CartComponent implements OnInit {
+  cart: product[] = [];
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart(1);
+  }
 
   get subtotal(): number {
     return this.cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
   }
 
-  updateQuantity(item: CartItem, quantity: number) {
+  updateQuantity(item: product, quantity: number) {
     item.quantity = quantity;
   }
 
   removeItem(itemId: number) {
-    this.cart = this.cart.filter((item) => item.id !== itemId);
+    this.cartService.removeProduct(1, itemId);
+    this.cart = this.cartService.getCart(1);
   }
 
   increaseQuantity(item: any) {
