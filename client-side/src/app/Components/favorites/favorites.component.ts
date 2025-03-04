ee83dd98dc1d9a422b11c1fdf024c1746bc56c2a
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-04 21:39:05 +0200

    implemented favorites page

--- client-side/src/app/Components/favorites/favorites.component.ts
+++ client-side/src/app/Components/favorites/favorites.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HeaderBannerComponent } from '../shared/header-banner/header-banner.component';
 import { FeatureBannerComponent } from '../shared/feature-banner/feature-banner.component';
 import { FavoriteService } from '../../Services/favorite.service';
@@ -6,6 +6,7 @@ import { product } from '../../Models/product.model';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../shared/button/button.component';
 import { RouterModule } from '@angular/router';
+import { CartService } from '../../Services/cart.service';
 
 @Component({
   selector: 'app-favorites',
@@ -21,11 +22,28 @@ import { RouterModule } from '@angular/router';
 })
 export class FavoritesComponent implements OnInit {
   favorites: product[] = [];
-  constructor(private favoriteService: FavoriteService) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private favoriteService: FavoriteService,
+    private cartService: CartService
+  ) {}
   ngOnInit(): void {
     this.favorites = this.favoriteService.getFavorites(1);
   }
   removeItem(id: number) {
     this.favoriteService.removeFavorite(1, id);
+  }
+
+  addToCart(product: product) {
+    if (this.isInCart(product.id)) {
+      this.cartService.removeProduct(1, product.id);
+    } else {
+      this.cartService.addProduct(1, product);
+    }
+    this.cdr.detectChanges(); // Force UI update
+  }
+
+  isInCart(productID: number): boolean {
+    return this.cartService.getCart(1).some((p) => p.id === productID);
   }
 }
