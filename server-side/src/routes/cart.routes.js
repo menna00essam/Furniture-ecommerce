442d28f28bdca2dd4f81558d0750a3c027f27dcd
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-04 13:30:50 +0200

    Implement cart controller

--- server-side/src/routes/cart.routes.js
+++ server-side/src/routes/cart.routes.js
@@ -1,7 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const cartController = require('../controllers/cart.controller');
 
-// 1- getall categories
-router.route("/").get().post().patch();
+router.route('/').get(cartController.getUserCart); // GET User Cart
+
+router
+  .route('/')
+  .post(cartController.addToCart) // Add Items
+  .patch(cartController.updateCart); // Update Items or Delete
 
 module.exports = router;
