3635bf13baa1dac360819aa3ec7dcefe4fa604c5
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-04 20:22:54 +0200

    used cuurency pipe

--- client-side/src/app/Components/favorites/favorites.component.ts
+++ client-side/src/app/Components/favorites/favorites.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderBannerComponent } from '../shared/header-banner/header-banner.component';
 import { FeatureBannerComponent } from '../shared/feature-banner/feature-banner.component';
+import { FavoriteService } from '../../Services/favorite.service';
+import { product } from '../../Models/product.model';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../shared/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [HeaderBannerComponent, FeatureBannerComponent],
+  imports: [
+    HeaderBannerComponent,
+    FeatureBannerComponent,
+    ButtonComponent,
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css',
 })
-export class FavoritesComponent {}
+export class FavoritesComponent implements OnInit {
+  favorites: product[] = [];
+  constructor(private favoriteService: FavoriteService) {}
+  ngOnInit(): void {
+    this.favorites = this.favoriteService.getFavorites(1);
+  }
+  removeItem(id: number) {
+    this.favoriteService.removeFavorite(1, id);
+  }
+}
