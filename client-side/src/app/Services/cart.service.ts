d70c4440dc0b5b0c94b43c3da1eb4de56d68874e
ahmedgamal5 <ahmedgamalflame5534@gmail.com>
2025-03-05 16:30:39 +0200

    update cart service

--- client-side/src/app/Services/cart.service.ts
+++ client-side/src/app/Services/cart.service.ts
@@ -1,25 +1,49 @@
 import { Injectable } from '@angular/core';
 import { product } from '../Models/product.model';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   private cart: product[] = [];
+  constructor(private productService: ProductService) {}
 
+  
   getCart(userId: number): product[] {
     return [...this.cart];
   }
 
   addProduct(userId: number, product: product): void {
-    if (this.cart.every((p) => p.id != product.id)) this.cart.push(product);
+    if (this.cart.every((p) => p.id != product.id)) this.cart.push({ ...product, quantity: 1 });
   }
 
   removeProduct(userId: number, productId: number): void {
     this.cart = this.cart.filter((p) => p.id !== productId);
   }
 
-  //To be done
-  increaseQuantity(userId: number, productId: number) {}
-  deccreaseQuantity(userId: number, productId: number) {}
+  increaseQuantity(productId: number): void {
+    const product = this.cart.find((p) => p.id === productId);
+    const stockProduct = this.getProductStock(productId); 
+  
+    if (product && stockProduct && product.quantity < stockProduct.quantity) {
+        product.quantity++;
+    }
+}
+  private getProductStock(productId: number): product | undefined {
+    return this.productService.getProduct(productId);
+}
+
+  decreaseQuantity(productId: number): void {
+    const product = this.cart.find((p) => p.id === productId);
+    if (product && product.quantity > 1) {
+      product.quantity--;
+    }
+  }
+
+
+  getSubtotal(): number {
+    return this.cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  }
+
 }
