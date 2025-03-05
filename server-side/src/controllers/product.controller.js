8734c6f83e9ea46bb16740b6d33d896729edf4bd
mariemhawary <mariemhawary843@gmail.com>
2025-03-05 15:33:49 +0200

    added get products by category

--- server-side/src/controllers/product.controller.js
+++ server-side/src/controllers/product.controller.js
@@ -46,6 +46,50 @@ const getAllProducts = asyncWrapper(async (req, res, next) => {
   });
 });
 
+const getProductsByCategory = asyncWrapper(async (req, res, next) => {
+  let categoryId = req.params;
+  let { limit = 16, page = 1, order = "desc" } = req.query;
+
+  // Ensure valid pagination inputs
+  limit = Math.max(1, limit);
+  page = Math.max(1, page);
+
+  if (!categoryId) {
+    return next(
+      new AppError("Category ID is required.", 400, httpStatusText.FAIL)
+    );
+  }
+
+  if (isNaN(limit) || isNaN(page)) {
+    return next(
+      new AppError(
+        "Invalid pagination parameters. 'limit' and 'page' must be positive numbers.",
+        400,
+        httpStatusText.FAIL
+      )
+    );
+  }
+
+  const skip = (page - 1) * limit;
+  const sortOrder = order === "asc" ? 1 : -1;
+  const totalProducts = await Product.countDocuments({ categoryId });
+
+  // Fetch products with pagination and sorting
+  const products = await Product.find({ categoryId })
+    .select(
+      "_id productName productSubtitle productImages productPrice productDate productSale"
+    )
+    .limit(limit)
+    .skip(skip)
+    .sort({ productDate: sortOrder });
+
+  res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: { totalProducts, products },
+  });
+});
+
 module.exports = {
   getAllProducts,
+  getProductsByCategory,
 };
--- server-side/src/routes/product.routes.js
+++ server-side/src/routes/product.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.route("/").get(productController.getAllProducts);
 router.route("/:product_id").get();
-router.route("/category/:category_id").get();
+router.route("/category/:category_id").get(productController.getProductsByCategory);
 router.route("/comparison").get(); // - get products full data for compariosn
 router.route("/all").get(); // - get all-products (name and id ) usred in search
 
