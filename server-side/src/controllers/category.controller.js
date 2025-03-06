26a5f46abfa93e53c35676f2fb2353237d32aa1d
ahmedgamal5 <ahmedgamalflame5534@gmail.com>
2025-03-06 02:08:43 +0200

    get all categories

--- server-side/src/controllers/category.controller.js
+++ server-side/src/controllers/category.controller.js
@@ -1,1 +1,26 @@
-// 1- getall category
+const Category = require("../models/category.model");
+const httpStatusText = require("../utils/httpStatusText");
+const AppError = require("../utils/appError");
+const asyncWrapper = require("../middlewares/asyncWrapper.middleware");
+
+
+const getAllCategories = asyncWrapper(async (req, res, next) => {
+  try {
+    const categories = await Category.find({}, { __v: false }); 
+    // console.log("categories>>>>>>>>>>>>>>",categories)
+    if (!categories.length) {
+      return next(new AppError("No categories found.", 404, httpStatusText.FAIL));
+    }
+
+    res.status(200).json({
+      status: httpStatusText.SUCCESS,
+      data: categories,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = {
+  getAllCategories,
+};
