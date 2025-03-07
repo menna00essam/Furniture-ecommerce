9402085c8c33fad71a050805350c14693151bb99
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-03-08 00:22:58 +0200

    posts refactor

--- server-side/src/controllers/post.controller.js
+++ server-side/src/controllers/post.controller.js
@@ -5,6 +5,7 @@ const httpStatusText = require("../utils/httpStatusText");
 const AppError = require("../utils/appError");
 const Post = require("../models/blog.model");
 const asyncWrapper = require("../middlewares/asyncWrapper.middleware");
+
 const getAllPosts = asyncWrapper(async (req, res, next) => {
   let { limit = 4, page = 1 } = req.query;
   limit = Math.max(1, limit);
@@ -22,18 +23,21 @@ const getAllPosts = asyncWrapper(async (req, res, next) => {
     .select("_id blogTitle description user image category date adminUser")
     .limit(limit)
     .skip(skip)
-    .sort({ date: -1 });
+    .sort({ date: -1 })
+    .lean();
 
   res.status(200).json({
     status: httpStatusText.SUCCESS,
     data: { totalPosts, posts },
   });
 });
+
 const getRecentPosts = asyncWrapper(async (req, res, next) => {
   const posts = await Post.find()
     .select("_id blogTitle description image tags")
     .limit(5)
-    .sort({ date: -1 });
+    .sort({ date: -1 })
+    .lean();
 
   res.status(200).json({
     status: httpStatusText.SUCCESS,
