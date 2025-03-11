a290ff0c8063eaff5472041492e1e304c8aa9ca7
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-11 23:23:38 +0200

    fixed payment method animation

--- client-side/src/app/Components/checkout/checkout.component.ts
+++ client-side/src/app/Components/checkout/checkout.component.ts
@@ -23,6 +23,7 @@ import { CartService } from '../../Services/cart.service';
 import { CurrencyPipe } from '@angular/common';
 import { PaymentComponent } from '../payment/payment.component';
 import { CheckoutService } from '../../Services/checkout.service';
+import { RouterModule } from '@angular/router';
 
 export function noNumbersValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
@@ -36,6 +37,7 @@ export function noNumbersValidator(): ValidatorFn {
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
     ReactiveFormsModule,
     HeaderBannerComponent,
     FeatureBannerComponent,
@@ -46,10 +48,10 @@ export function noNumbersValidator(): ValidatorFn {
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
   animations: [
-    trigger('fadeInOut', [
-      state('void', style({ opacity: 0, height: '0' })),
-      state('*', style({ opacity: 1, height: '*' })),
-      transition('void <=> *', animate('0.5s ease-in-out')),
+    trigger('slideUpDown', [
+      state('in', style({ height: '*', opacity: 1 })),
+      state('out', style({ height: '0', opacity: 0 })),
+      transition('in <=> out', animate('200ms ease-in-out')),
     ]),
   ],
 })
@@ -100,8 +102,11 @@ export class CheckoutComponent {
       errors: [],
     };
 
-    if ((this.selectedPayment === 'bank'||this.selectedPayment === 'cod') && this.paymentComponent) {
-      this.paymentComponent.paymentForm.markAllAsTouched(); 
+    if (
+      (this.selectedPayment === 'bank' || this.selectedPayment === 'cod') &&
+      this.paymentComponent
+    ) {
+      this.paymentComponent.paymentForm.markAllAsTouched();
       paymentValidation = this.checkoutService.validatePaymentForm(
         this.paymentComponent.paymentForm.value
       );
