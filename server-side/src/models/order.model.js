aa42eb6cc6bb3c09cbc8736d6a7c95a238c87022
mariemhawary <mariemhawary843@gmail.com>
2025-03-13 05:45:41 +0200

    handled the automatic order number generate

--- server-side/src/models/order.model.js
+++ server-side/src/models/order.model.js
@@ -35,9 +35,20 @@ const OrderSchema = new mongoose.Schema(
       enum: ["Direct Bank Transfer", "Cash on Delivery"],
       required: true,
     },
+    orderNumber: { type: String, unique: true },
+
     transactionId: { type: String },
   },
   { timestamps: true }
 );
+// Middleware to generate orderNumber before saving
 
+OrderSchema.pre("save", async function (next) {
+  if (!this.orderNumber) {
+    this.orderNumber = `${Date.now()}-${Math.floor(1000 + Math.random() * 9000)
+      .toString()
+      .slice(0, 5)}`;
+  }
+  next();
+});
 module.exports = mongoose.model("Order", OrderSchema);
