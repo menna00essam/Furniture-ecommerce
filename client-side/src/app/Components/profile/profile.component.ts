b39ae73c5f3b3036a18e3f3e4fec61732fe569f6
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-03-20 02:49:29 +0200

    fixed profile UI

--- client-side/src/app/Components/profile/profile.component.ts
+++ client-side/src/app/Components/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../Services/user.service';
 import { user } from '../../Models/user.model';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, ActivatedRoute, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../Services/auth.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
@@ -15,25 +15,30 @@ import { CommonModule } from '@angular/common';
 export class ProfileComponent implements OnInit {
   user$!: Observable<user | null>;
   isChecked: boolean = false;
-  activeComponent: string = 'orders';
+  activeComponent: string = '';
 
   constructor(
     private userService: UserService,
     private router: Router,
+    private route: ActivatedRoute,
     private authService: AuthService
   ) {}
 
   ngOnInit(): void {
     this.user$ = this.userService.user$;
     this.userService.getUser().subscribe();
+
+    // Get active route dynamically
+    this.route.firstChild?.url.subscribe((segments) => {
+      this.activeComponent = segments.length ? segments[0].path : 'orders';
+    });
   }
 
   toggleCheck() {
     this.isChecked = !this.isChecked;
   }
 
-  route(r: string) {
-    this.activeComponent = r;
+  routeTo(r: string) {
     this.router.navigate([`/profile/${r}`]);
   }
 
