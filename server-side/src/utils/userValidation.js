8ee5276e9aab1288e19b7177a690655f755e124d
Ammar942 <ammaratef919@gmail.com>
2025-04-01 10:26:15 +0200

    feat: handle userValidation

--- server-side/src/utils/userValidation.js
+++ server-side/src/utils/userValidation.js
@@ -1,21 +1,22 @@
-const Ajv = require('ajv');
+const Ajv = require("ajv");
 const ajv = new Ajv();
 
 // User schema validation using Ajv library
 const userSchema = {
-  type: 'object',
+  type: "object",
   properties: {
-    username: { type: 'string', pattern: '^[a-zA-Z]*$', minLength: 3 },
+    username: { type: "string", pattern: "^[a-zA-Z]*$", minLength: 3 },
     email: {
-      type: 'string',
-      pattern: '^[a-zA-Z0-9._]+@(gmail|yahoo|outlook)+.[a-z]{2,4}$',
+      type: "string",
+      pattern: "^[a-zA-Z0-9._]+@(gmail|yahoo|outlook)+.[a-z]{2,4}$",
     },
-    googleId: { type: 'string' },
-    password: { type: 'string', minLength: 8 },
-    phone: { type: 'string', pattern: '^[0-9]{11}$' },
-    role: { type: 'string', enum: ['ADMIN', 'USER'], default: 'USER' },
+    googleId: { type: "string" },
+    password: { type: "string", minLength: 8 },
+    phone: { type: "string", pattern: "^[0-9]{11}$" },
+    role: { type: "string", enum: ["ADMIN", "USER"], default: "USER" },
+    agree: { type: "boolean" },
   },
-  required: ['username', 'email'],
+  required: ["username", "email"],
   additionalProperties: false,
 };
 module.exports = ajv.compile(userSchema);
