9d509ab7abf08e54563f3e31c933dd74ac5f5c90
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-04-03 00:38:40 +0200

    handled cart and favorites modals

--- client-side/src/app/Components/root/header/header.component.ts
+++ client-side/src/app/Components/root/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ElementRef, OnInit } from '@angular/core';
+import { Component, HostListener, ElementRef, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +21,8 @@ import { NavigationComponent } from './navigation/navigation.component';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  @Output() openFavorites = new EventEmitter<void>();
+  @Output() openCart = new EventEmitter<void>();
   isActive = false;
 
   constructor(private router: Router, private elementRef: ElementRef) {}
@@ -31,15 +33,5 @@ export class HeaderComponent {
 
   goToProduct(id: string) {
     this.router.navigate([`/product/${id}`]);
-  }
-
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: Event) {
-    if (
-      this.isActive &&
-      !this.elementRef.nativeElement.contains(event.target)
-    ) {
-      this.isActive = false;
-    }
   }
 }
--- client-side/src/app/Components/root/header/user-actions/user-actions.component.ts
+++ client-side/src/app/Components/root/header/user-actions/user-actions.component.ts
@@ -1,49 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule, CurrencyPipe } from '@angular/common';
-import { trigger, transition, animate, style } from '@angular/animations';
-import { RouterModule } from '@angular/router';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { product } from '../../../../Models/product.model';
 import { FavoriteService } from '../../../../Services/favorite.service';
 import { CartService } from '../../../../Services/cart.service';
 import { AuthService } from '../../../../Services/auth.service';
-import { productCart } from '../../../../Models/productCart.model';
-import { productFavorite } from '../../../../Models/productFavorite.model';
-import { user } from '../../../../Models/user.model';
 import { UserService } from '../../../../Services/user.service';
+import { user } from '../../../../Models/user.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-actions',
-  imports: [CurrencyPipe, RouterModule, CommonModule],
   templateUrl: './user-actions.component.html',
-  animations: [
-    trigger('slideInOut', [
-      transition(':enter', [
-        style({ transform: 'translateX(100%)' }),
-        animate(
-          '0.5s cubic-bezier(.4,0,.2,1)',
-          style({ transform: 'translateX(0%)' })
-        ),
-      ]),
-      transition(':leave', [
-        animate(
-          '0.5s cubic-bezier(.4,0,.2,1)',
-          style({ transform: 'translateX(100%)' })
-        ),
-      ]),
-    ]),
-  ],
+  imports: [CommonModule, RouterModule],
 })
 export class UserActionsComponent implements OnInit {
+  @Output() openFavorites = new EventEmitter<void>();
+  @Output() openCart = new EventEmitter<void>();
+
   user$!: Observable<user | null>;
-  cartProductsTotalPrice!: Observable<number>;
   cartLength$!: Observable<number>;
-  cart$!: Observable<productCart[]>;
-  favorites$!: Observable<productFavorite[]>;
   favoritesLength$!: Observable<number>;
-  favModalShow = false;
-  cartModalShow = false;
   isLoggedIn = false;
 
   constructor(
@@ -55,44 +32,22 @@ export class UserActionsComponent implements OnInit {
 
   ngOnInit(): void {
     this.user$ = this.userService.user$;
-    this.userService.getUser().subscribe((user) => {
-      console.log(user.thumbnail);
-    });
+
     this.authService.isLoggedIn$.subscribe((status) => {
       this.isLoggedIn = status;
     });
 
-    this.cart$ = this.cartService.cart$;
     this.cartLength$ = this.cartService.cart$.pipe(map((cart) => cart.length));
-    this.cartProductsTotalPrice = this.cartService.getSubtotal();
-
-    this.favorites$ = this.favoriteService.favorites$;
     this.favoritesLength$ = this.favoriteService.favorites$.pipe(
       map((favorites) => favorites.length)
     );
   }
 
-  deleteFavorite(id: string): void {
-    this.favoriteService.toggleFavourite(id).subscribe();
+  showFavModal() {
+    this.openFavorites.emit();
   }
 
-  deleteCartProduct(id: string): void {
-    this.cartService.removeProduct(id);
-  }
-
-  toggleFavModal(open: boolean): void {
-    this.favModalShow = open;
-    this.toggleBodyScroll(open);
-  }
-
-  toggleCartModal(open: boolean): void {
-    this.cartModalShow = open;
-    this.toggleBodyScroll(open);
-    console.log('toggleCartModal', open);
-  }
-
-  private toggleBodyScroll(isOpen: boolean): void {
-    // document.body.style.overflowY = isOpen ? 'hidden' : 'auto';
-    // document.body.style.width = isOpen ? '100%' : '';
+  showCartModal() {
+    this.openCart.emit();
   }
 }
