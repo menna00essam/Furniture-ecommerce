e1fec2e48eec152efbfc0c7b35aad81b8687a24f
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-04-05 15:13:07 +0200

    fixing be-checkout -api

--- server-side/src/controllers/checkout.controller.js
+++ server-side/src/controllers/checkout.controller.js
@@ -1,58 +1,46 @@
-const mongoose = require('mongoose');
-const httpStatusText = require('../utils/httpStatusText');
-const AppError = require('../utils/appError');
+const mongoose = require("mongoose");
+const httpStatusText = require("../utils/httpStatusText");
+const AppError = require("../utils/appError");
 
-const Cart = require('../models/cart.model');
-const Order = require('../models/order.model');
-const Product = require('../models/product.model');
+const Cart = require("../models/cart.model");
+const Order = require("../models/order.model");
+const Product = require("../models/product.model");
 
-const asyncWrapper = require('../middlewares/asyncWrapper.middleware');
+const asyncWrapper = require("../middlewares/asyncWrapper.middleware");
 
 const placeOrder = asyncWrapper(async (req, res, next) => {
-  try{
-  const userId = req.user._id;
-  const { shippingAddress, paymentMethod, transactionId} = req.body;
-  console.log("req.body>>>>>>>>>>",req.body); 
-  if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return next(new AppError("Invalid User ID", 400, httpStatusText.FAIL));
-  }
-  
-  const cart = await Cart.findOne({ userId }).populate("products.productId");
-  if (!cart || cart.products.length === 0) {
-    return next(new AppError("Cart is empty", 400, httpStatusText.FAIL));
-  }
-  const orderItems = cart.products.map((item) => {
-    const effectivePrice = item.productId.productPrice * (1 - item.productId.productSale / 100);
-    return {
-      productId: item.productId._id,
-      productName: item.productId.productName,
-      quantity: item.quantity,
-      price: effectivePrice,
-      subtotal: effectivePrice * item.quantity,
-    };
-  });
-  console.log("-----------------------------");
-  console.log("cart.products>>>>>>>>>>>",cart.products); 
-  console.log("-----------------------------");
-  console.log("orderItems>>>>>>>>>>>",orderItems); 
-  console.log("-----------------------------");
-  console.log("cart>>>>>>>>>>>",cart); 
-
-    const cart = await Cart.findOne({ userId }).populate('products.productId');
-    if (!cart || cart.products.length === 0) {
-      return next(new AppError('Cart is empty', 400, httpStatusText.FAIL));
+  try {
+    const userId = req.user._id;
+    const { shippingAddress, paymentMethod, transactionId } = req.body;
+    console.log("req.body>>>>>>>>>>", req.body);
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return next(new AppError("Invalid User ID", 400, httpStatusText.FAIL));
     }
 
-    const orderItems = cart.products.map((item) => ({
-      productId: item.productId._id,
-      productName: item.productId.productName,
-      quantity: item.quantity,
-      price: item.productId.productPrice,
-      subtotal: item.subtotal,
-    }));
+    const cart = await Cart.findOne({ userId }).populate("products.productId");
+    if (!cart || cart.products.length === 0) {
+      return next(new AppError("Cart is empty", 400, httpStatusText.FAIL));
+    }
+    const orderItems = cart.products.map((item) => {
+      const effectivePrice =
+        item.productId.productPrice * (1 - item.productId.productSale / 100);
+      return {
+        productId: item.productId._id,
+        productName: item.productId.productName,
+        quantity: item.quantity,
+        price: effectivePrice,
+        subtotal: effectivePrice * item.quantity,
+      };
+    });
+    console.log("-----------------------------");
+    console.log("cart.products>>>>>>>>>>>", cart.products);
+    console.log("-----------------------------");
+    console.log("orderItems>>>>>>>>>>>", orderItems);
+    console.log("-----------------------------");
+    console.log("cart>>>>>>>>>>>", cart);
 
     for (const item of cart.products) {
-      console.log('quantiti', item.quantity, item.productId.productQuantity);
+      console.log("quantiti", item.quantity, item.productId.productQuantity);
       if (item.quantity > item.productId.productQuantity) {
         return next(
           new AppError(
@@ -63,16 +51,18 @@ const placeOrder = asyncWrapper(async (req, res, next) => {
         );
       }
     }
-  }
-  const totalAmount = orderItems.reduce((acc, item) => acc + item.subtotal, 0);
-  const order = new Order({
-    userId,
-    orderItems,
-    shippingAddress,
-    totalAmount,
-    paymentMethod,
-    transactionId,
-
+    const totalAmount = orderItems.reduce(
+      (acc, item) => acc + item.subtotal,
+      0
+    );
+    const order = new Order({
+      userId,
+      orderItems,
+      shippingAddress,
+      totalAmount,
+      paymentMethod,
+      transactionId,
+    });
 
     await order.save();
 
@@ -86,11 +76,11 @@ const placeOrder = asyncWrapper(async (req, res, next) => {
     await Promise.all(updatePromises);
     await Cart.findOneAndDelete({ userId });
 
-    res.status(201).json({ message: 'Order placed successfully!', order });
+    res.status(201).json({ message: "Order placed successfully!", order });
   } catch (error) {
-    console.error('Error placing order:', error);
+    console.error("Error placing order:", error);
     return next(
-      new AppError('Failed to place order', 500, httpStatusText.ERROR)
+      new AppError("Failed to place order", 500, httpStatusText.ERROR)
     );
   }
 });
