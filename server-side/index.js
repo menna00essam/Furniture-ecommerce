d0172941327a0fca182db8235502043dc41ae000
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-04-10 12:51:11 +0200

    added vercel configuration

--- server-side/index.js
+++ server-side/index.js
@@ -1,37 +1,37 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
+const cors = require('cors');
+require('dotenv').config();
 
 // Scripts to run
-require("./src/middlewares/passport.middleware");
-require("./src/services/orderStatus.service");
+require('./src/middlewares/passport.middleware');
+require('./src/services/orderStatus.service');
 
-const passport = require("passport");
+const passport = require('passport');
 
 / * * * * Utils * * * * /;
-const httpStatusText = require("./src/utils/httpStatusText");
+const httpStatusText = require('./src/utils/httpStatusText');
 / * * * * End Utils * * * * /;
 
 const PORT = process.env.PORT || 5000;
 app.use(passport.initialize());
 / * * * * DB * * * /;
-const connectDB = require("./src/config/db");
+const connectDB = require('./src/config/db');
 / * * * * End Db * * * * /;
 
 / * * * * Router imports * * * * /;
 
-const registerationRouter = require("./src/routes/registration.routes");
-const userRouter = require("./src/routes/user.routes");
-const categoreRouter = require("./src/routes/category.routes");
-const productRouter = require("./src/routes/product.routes");
-const postRouter = require("./src/routes/post.routes");
-const checkoutRouter = require("./src/routes/checkout.routes");
-const cartRouter = require("./src/routes/cart.routes");
-const galleryRouter = require("./src/routes/gallery.routes");
-const contactRouter = require("./src/routes/contact.routes");
-const orderRouter = require("./src/routes/order.routes");
-const paymentRouter = require("./src/routes/payment.routes");
+const registerationRouter = require('./src/routes/registration.routes');
+const userRouter = require('./src/routes/user.routes');
+const categoreRouter = require('./src/routes/category.routes');
+const productRouter = require('./src/routes/product.routes');
+const postRouter = require('./src/routes/post.routes');
+const checkoutRouter = require('./src/routes/checkout.routes');
+const cartRouter = require('./src/routes/cart.routes');
+const galleryRouter = require('./src/routes/gallery.routes');
+const contactRouter = require('./src/routes/contact.routes');
+const orderRouter = require('./src/routes/order.routes');
+const paymentRouter = require('./src/routes/payment.routes');
 
 / * * * * End Router imports * * * * /;
 
@@ -43,29 +43,29 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json("You need furniture? Here’s Furniro!");
+app.get('/', (req, res) => {
+  res.json('You need furniture? Here’s Furniro!');
 });
 
 / * * * Routes * * * /;
 
-app.use("/auth", registerationRouter);
-app.use("/users", userRouter);
-app.use("/categories", categoreRouter);
-app.use("/products", productRouter);
-app.use("/posts", postRouter);
-app.use("/checkout", checkoutRouter);
-app.use("/cart", cartRouter);
-app.use("/api", galleryRouter);
-app.use("/contact", contactRouter);
-app.use("/orders", orderRouter);
-app.use("/payments", paymentRouter);
+app.use('/auth', registerationRouter);
+app.use('/users', userRouter);
+app.use('/categories', categoreRouter);
+app.use('/products', productRouter);
+app.use('/posts', postRouter);
+app.use('/checkout', checkoutRouter);
+app.use('/cart', cartRouter);
+app.use('/api', galleryRouter);
+app.use('/contact', contactRouter);
+app.use('/orders', orderRouter);
+app.use('/payments', paymentRouter);
 
 / * * * Global MiddleWare * * * /;
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   return res.status(404).json({
     status: httpStatusText.ERROR,
-    message: "this resource is not avilable",
+    message: 'this resource is not avilable',
   });
 });
 // global error handlers
@@ -80,3 +80,4 @@ app.use((error, req, res, next) => {
 app.listen(PORT, () =>
   console.log(`I am running on: http://localhost:${PORT}`)
 );
+module.exports = app;
