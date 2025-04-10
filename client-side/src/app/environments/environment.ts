196cab706cc3566a0a7477a91fcaa264fccf3b96
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-04-10 13:12:30 +0200

    deployment

--- client-side/src/app/environments/environment.ts
+++ client-side/src/app/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000',
-    stripePublishableKey: 'pk_test_51R8DXEFyZOGECPazU6siO2Pkx8aqbuLlcIt7lpI3ARJS50bV4IJmnlzIy8YKrwonWNPQwOJy9VWl6spYTkUdTccV00Rt3WgJA2'
-  };
-
+  production: false,
+  // apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://furniture-ecommerce-phi.vercel.app',
+  stripePublishableKey:
+    'pk_test_51R8DXEFyZOGECPazU6siO2Pkx8aqbuLlcIt7lpI3ARJS50bV4IJmnlzIy8YKrwonWNPQwOJy9VWl6spYTkUdTccV00Rt3WgJA2',
+};
