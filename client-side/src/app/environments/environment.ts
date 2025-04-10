59fb96c0277491f10af5f258c42b12c61fe1cc76
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-04-10 14:28:36 +0200

    production

--- client-side/src/app/environments/environment.ts
+++ client-side/src/app/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:5000',
   apiUrl: 'https://furniture-ecommerce-phi.vercel.app',
   stripePublishableKey:
     'pk_test_51R8DXEFyZOGECPazU6siO2Pkx8aqbuLlcIt7lpI3ARJS50bV4IJmnlzIy8YKrwonWNPQwOJy9VWl6spYTkUdTccV00Rt3WgJA2',
